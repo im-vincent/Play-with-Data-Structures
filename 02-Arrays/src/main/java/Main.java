d684c543c88bbc889b94b63cc818b3209cedbbaa
xi Wang <vincent_wangxi@126.com>
2019-12-24 13:25:54 +0800

    使用泛型

---
+++ 02-Arrays/src/main/java/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = i;
+
+        int[] scores = new int[]{100, 99, 66};
+        for (int i = 0; i < scores.length; i++)
+            System.out.println(scores[i]);
+
+        for (int score : scores)
+            System.out.println(score);
+
+        scores[0] = 96;
+
+        for (int i = 0; i < scores.length; i++)
+            System.out.println(scores[i]);
+
+
+        Array<Integer> array = new Array<>(20);
+        for (int i = 0; i < 10; i++) {
+            array.addLast(i);
+            System.out.println(array);
+        }
+
+        array.add(1, 100);
+        System.out.println(array);
+
+        array.addFirst(-1);
+        System.out.println(array);
+
+        array.remove(2);
+        System.out.println(array);
+
+        array.removeFirst();
+        System.out.println(array);
+
+        array.removeElement(8);
+        System.out.println(array);
+    }
+
+
+}
