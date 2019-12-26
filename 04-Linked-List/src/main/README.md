72a4ebd2b0158f1e4da562d5c028c75d8b5f9a4b
xi Wang <vincent_wangxi@126.com>
2019-12-26 15:40:50 +0800

    add remove

--- 04-Linked-List/src/main/README.md
+++ 04-Linked-List/src/main/README.md
@@ -1,3 +1,6 @@
 ![](https://raw.githubusercontent.com/im-vincent/image/master/20191226110712.png)
 ![](https://raw.githubusercontent.com/im-vincent/image/master/20191226110838.png)
 ![](https://raw.githubusercontent.com/im-vincent/image/master/20191226111540.png)
+
+![](https://raw.githubusercontent.com/im-vincent/image/master/20191226145816.png)
+
--- 04-Linked-List/src/main/java/LinkedList.java
+++ 04-Linked-List/src/main/java/LinkedList.java
@@ -19,10 +19,7 @@ public class LinkedList<E> {
 
         @Override
         public String toString() {
-            return "Node{" +
-                    "e=" + e +
-                    ", next=" + next +
-                    '}';
+            return e.toString();
         }
     }
 
@@ -66,6 +63,115 @@ public class LinkedList<E> {
 
     public void addLast(E e) {
         add(size, e);
+    }
+
+    // 获得链表的第index(0-based)个位置的元素
+    // 在链表中不是一个常用的操作，练习用：）
+    public E get(int index) {
+
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        return cur.e;
+    }
+
+    // 获得链表的第一个元素
+    public E getFirst() {
+        return get(0);
+    }
+
+    // 获得链表的最后一个元素
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    // 修改链表的第index(0-based)个位置的元素为e
+    // 在链表中不是一个常用的操作，练习用：）
+    public void set(int index, E e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    // 查找链表中是否有元素e
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    // 从链表中删除index(0-based)位置的元素, 返回删除的元素
+    // 在链表中不是一个常用的操作，练习用：）
+    public E remove(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++)
+            prev = prev.next;
+
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size--;
+
+        return retNode.e;
+    }
+
+    // 从链表中删除第一个元素, 返回删除的元素
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    // 从链表中删除最后一个元素, 返回删除的元素
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    // 从链表中删除元素e
+    public void removeElement(E e) {
+
+        Node prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.e.equals(e))
+                break;
+            prev = prev.next;
+        }
+
+        if (prev.next != null) {
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            size--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+
+//        Node cur = dummyHead.next;
+//        while(cur != null){
+//            res.append(cur + "->");
+//            cur = cur.next;
+//        }
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next)
+            res.append(cur + "->");
+        res.append("NULL");
+
+        return res.toString();
     }
 }
 
